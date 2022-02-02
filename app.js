466f904ca9f14597cfb3c99e29921279730a9946
Caroline <caroline_rooney@icloud.com>
2022-02-02 09:44:03 +0000

    fe and be communicating

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req,res) => {
-    res.send("Hello World");
+    res.json({message:"Hello World"});
 });
 
 app.listen(PORT, () => {
