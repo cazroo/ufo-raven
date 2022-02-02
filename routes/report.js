46739c64a72ccff3f9c02139723e3d7084a5af58
D.Loman <donnaloman@gmail.com>
2022-02-02 17:02:37 +0000

    bug in report model

---
+++ routes/report.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const Report = require("../models/report");
+
+// get all reports 
+router.get("/", async (req, res) => {
+    const allReports = await Report.findAll({
+        attributes: ["id", "title", "time", "location", "description"]
+    });
+    res.status(200).json({msg: "All reports found", data: allReports});
+})
+
+// get single report
+router.get("/:id", async (req, res) => {
+    const report = await Report.findOne({where: {id: req.params.id}});
+    res.status(200).json({msg: `${report} found`});
+})
+
+// delete all reports
+router.delete("/", async (req, res) => {
+    const deletedReport = await Report.destroy({where:{}});
+    res.status(200).json({msg:`deleted ${deletedReport}`});
+})
+
+//delete single report
+router.delete("/:id", async(req, res) => {
+    const report = await Report.findOne({where: {id: req.params.id}});
+    const deletedReport = await report.destroy();
+    console.log(deletedReport)
+    res.status(200).json({msg: deletedReport});
+});
+
+//update single report
+router.put("/:id", async(req, res) => {
+    const updatedReport = await Report.update({
+        title: req.body.title, 
+        time: req.body.time,
+        location: req.body.location,
+        description: req.body.description
+    }, {where: {id: req.params.id}} );
+    const report = await Report.findOne({where: {id: req.params.id}});
+    res.status(202).json({msg:`${report} updated successfully`});
+})
+
+// create individual report
+router.post("/", async (req, res) => {
+    const report = await Report.create({
+        title: req.body.title, 
+        time: req.body.time,
+        location: req.body.location,
+        description: req.body.description
+    });
+    req.status(201).json({msg:`${report} created`});
+})
+
