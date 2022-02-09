37ce727c171d6fcc5d326fd4bb71b69236a400da
Peter Marton <zsuzsu4588@freemail.hu>
2022-02-09 16:04:15 +0000

    personal-report-working

--- routes/report.js
+++ routes/report.js
@@ -1,22 +1,23 @@
 const router = require("express").Router();
 const Report = require("../models/report");
 const Location = require("../models/location");
+const User = require("../models/user");
 
 // get all reports  (checked & worked)
 router.get("/", async (req, res) => {
-    const allReports = await Report.findAll({
-        // attributes: ["id", "date", "description", "locationId"]
-        include: Location
-    });
-   
-    res.status(200).json({msg: "All reports found", data: allReports});
-})
+  const allReports = await Report.findAll({
+    // attributes: ["id", "date", "description", "locationId"]
+    include: Location
+  });
+
+  res.status(200).json({ msg: "All reports found", data: allReports });
+});
 
 // get single report (checked & worked)
 router.get("/:id", async (req, res) => {
-    const report = await Report.findOne({where: {id: req.params.id}});
-    res.status(200).json({msg: `Report found`, data: report});
-})
+  const report = await Report.findOne({ where: { id: req.params.id } });
+  res.status(200).json({ msg: `Report found`, data: report });
+});
 
 // // delete all reports (checked & worked)
 // router.delete("/", async (req, res) => {
@@ -25,24 +26,25 @@ router.get("/:id", async (req, res) => {
 // })
 
 //delete single report (checked & worked)
-router.delete("/:id", async(req, res) => {
-    const report = await Report.findOne({where: {id: req.params.id}});
-    const deletedReport = await Report.destroy({where:{id: req.params.id}});
-    res.status(200).json({msg: "Deleted report", data: deletedReport});
+router.delete("/:id", async (req, res) => {
+  const report = await Report.findOne({ where: { id: req.params.id } });
+  const deletedReport = await Report.destroy({ where: { id: req.params.id } });
+  res.status(200).json({ msg: "Deleted report", data: deletedReport });
 });
 
 //update single report (checked & worked)
-router.put("/:id", async(req, res) => {
-    const updatedReport = await Report.update(req.body, {where: {id: req.params.id}} );
-    const report = await Report.findOne({where: {id: req.params.id}});
-    res.status(202).json({msg:`${report} updated successfully`});
-})
+router.put("/:id", async (req, res) => {
+  const updatedReport = await Report.update(req.body, {
+    where: { id: req.params.id },
+  });
+  const report = await Report.findOne({ where: { id: req.params.id } });
+  res.status(202).json({ msg: `${report} updated successfully` });
+});
 
 // create individual report (checked & worked)
 router.post("/", async (req, res) => {
-    const report = await Report.create(req.body);
-    res.status(201).json({msg:`report created`, report});
-})
+  const report = await Report.create(req.body);
+  res.status(201).json({ msg: `report created`, report });
+});
 
 module.exports = router;
-
