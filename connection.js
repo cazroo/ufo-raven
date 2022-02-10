dc5b189e6b5ec1f05087c63d4742ffd037014bd9
cazroo <caroline_rooney@icloud.com>
2022-02-10 18:18:34 +0000

    connection

--- connection.js
+++ connection.js
@@ -2,7 +2,16 @@ const { Sequelize } = require("sequelize");
 
 let connection;
 if (process.env.NODE_ENV === "production") {
-  // heroku connection
+  connection = new Sequelize(`${process.env.DATABASE_URL}?sslmode=no-verify`, {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else if (process.env.NODE_ENV === "development") {
   connection = new Sequelize(process.env.DATABASE_URL, {
     logging: false,
